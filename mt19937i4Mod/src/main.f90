d0bf020cdbd552dd060e030a8311df27a22c4e87
wfw-pgr <wfw.pgr@gmail.com>
2022-09-24 18:17:22 +0900

    modify mt19937i4Mod

--- mt19937i4Mod/src/main.f90
+++ mt19937i4Mod/src/main.f90
@@ -2,12 +2,15 @@ program main
   use mt19937i4Mod, only : sgrnd, grnd, multi_sgrnd, multi_grnd
   use mt19937i4Mod, only : mt, multi_mt, mti, multi_mti
   implicit none
-  integer, parameter :: size = 10
-  integer, parameter :: seed = 20
-  integer, parameter :: OMPNumThreads = 4
-  integer            :: seeds(OMPNumThreads)
-  integer            :: i, myth
-  double precision   :: res(size), res_omp(OMPNumThreads,size)
+  integer, parameter    :: size = 10
+  integer, parameter    :: seed = 100
+  integer, parameter    :: max_nOMP = 10
+  integer               :: seeds(max_nOMP)
+  integer               :: i, myth, nOMP
+  double precision      :: res(size), res_omp(max_nOMP,size)
+  character(50)         :: c_size, fmt
+
+  nOMP = max_nOMP
 
   ! ------------------------------------------------------ !
   ! --- [1] initialization                             --- !
@@ -15,10 +18,10 @@ program main
   res(:) = 0.d0
   call sgrnd( seed )
   
-  do i=1, OMPNumThreads
-     seeds(i) = seed + (i-1) * 1
+  do i=1, max_nOMP
+     seeds(i) = seed + (i-1)
   enddo
-  call multi_sgrnd( seeds )
+  call multi_sgrnd( seeds, max_nOMP )
 
   ! ------------------------------------------------------ !
   ! --- [2] display mti & mt ( internal num. table )   --- !
@@ -34,7 +37,7 @@ program main
   do i=1, size
      res(i) = grnd()
   enddo
-  do myth=1, OMPNumThreads
+  do myth=1, nOMP
      do i=1, size
         res_omp(myth,i) = multi_grnd( myth )
      enddo
@@ -44,7 +47,9 @@ program main
   ! --- [4] write result                               --- !
   ! ------------------------------------------------------ !
   do i=1, size
-     write(6,"(i4,1x,5(f10.5,1x))") i, res(i), ( res_omp(myth,i), myth=1, OMPNumThreads )
+     write(c_size,"(i4)") nOMP+1
+     fmt = "(i4,1x," // trim(c_size)  // "(f10.5,1x))"
+     write(6,trim(fmt)) i, res(i), ( res_omp(myth,i), myth=1, nOMP )
   enddo
   
 
--- mt19937i4Mod/src/mt19937i4Mod.f90
+++ mt19937i4Mod/src/mt19937i4Mod.f90
@@ -1,25 +1,26 @@
 module mt19937i4Mod
   implicit none
-  integer         , parameter :: OMPNumThreads = 4
-  integer         , parameter :: N         = 624
-  integer         , parameter :: M         = 397
-  integer(8)      , parameter :: matA      = -1727483681_8
-  integer(8)      , parameter :: upperMask = -2147483648_8
-  integer(8)      , parameter :: lowerMask = +2147483647_8
-  integer(8)      , parameter :: maskB     = -1658038656_8
-  integer(8)      , parameter :: maskC     = -272236544_8
-  integer(8)      , parameter :: maskI     = 1812433253_8
-  integer         , parameter :: maskF     = -1
-  integer         , parameter :: mask1     = +1
-  integer         , parameter :: s_shift   =  7
-  integer         , parameter :: t_shift   =  15
-  integer         , parameter :: u_shift   = -11
-  integer         , parameter :: f_shift   = -18
-  integer                     :: mti       = N+1
+  integer         , parameter :: N            = 624
+  integer         , parameter :: M            = 397
+  integer(8)      , parameter :: matA         = -1727483681_8
+  integer(8)      , parameter :: upperMask    = -2147483648_8
+  integer(8)      , parameter :: lowerMask    = +2147483647_8
+  integer(8)      , parameter :: maskB        = -1658038656_8
+  integer(8)      , parameter :: maskC        = -272236544_8
+  integer(8)      , parameter :: maskI        = 1812433253_8
+  integer         , parameter :: maskF        = -1
+  integer         , parameter :: mask1        = +1
+  integer         , parameter :: s_shift      =  7
+  integer         , parameter :: t_shift      =  15
+  integer         , parameter :: u_shift      = -11
+  integer         , parameter :: f_shift      = -18
+  integer         , parameter :: default_seed = 4357
+  integer                     :: mti          = N+1
   integer                     :: mt(0:N-1)
-  integer(8)                  :: mag01      (0:1) = (/0_8,matA/)
-  integer                     :: multi_mt (0:N-1,OMPNumThreads)
-  integer                     :: multi_mti(OMPNumThreads) = N+1
+  integer(8)                  :: mag01(0:1)   = (/0_8,matA/)
+  integer                     :: nOMP         = 0
+  integer, allocatable        :: multi_mt (:,:)
+  integer, allocatable        :: multi_mti(:)
   double precision, parameter :: two_to_32 = 2.d0**32
 
 contains
@@ -54,13 +55,12 @@ contains
   function grnd()
     implicit none
     integer                     :: y, kk
-    integer, parameter          :: seed_default = 4357
     double precision            :: grnd
 
     if ( mti >= N ) then
        
        if ( mti == N+1 ) then
-          call sgrnd( seed_default )
+          call sgrnd( default_seed )
        endif
        
        do kk=0, N-M-1
@@ -100,19 +100,39 @@ contains
   ! ====================================================== !
   ! === multithreaded ver. of MT19937  ( setting )     === !
   ! ====================================================== !
-  subroutine multi_sgrnd( seeds )
+  subroutine multi_sgrnd( seeds, nSeeds )
     !$ use omp_lib
     implicit none
-    integer,intent(in) :: seeds(OMPNumThreads)
-    integer            :: k, kmti
+    integer, intent(in) :: nSeeds
+    integer, intent(in) :: seeds(nSeeds)
+    integer             :: k, kmti
+    integer             :: numthreads = 1
+
+    ! ------------------------------------------------------ !
+    ! --- [1] store nOMP and allocate multi_mt array     --- !
+    ! ------------------------------------------------------ !
+    if ( nOMP == 0 ) then
+       !$omp parallel shared(numthreads)
+       !$ numthreads = omp_get_num_threads()
+       !$omp end parallel
+       nOMP = numthreads
+       write(6,*)
+       write(6,*) "[mt19937i4Mod.f90] auto determination of nOMP == OMP_NUM_THREADS"
+       write(6,*) "[mt19937i4Mod.f90]            OMP_NUM_THREADS == ", nOMP
+       write(6,*)
+    endif
+    if ( .not.( allocated( multi_mt ) ) ) then
+       allocate( multi_mt (0:N-1,nOMP), source=0   )
+       allocate( multi_mti(      nOMP), source=N+1 )
+    endif
     
     ! ------------------------------------------------------ !
-    ! --- [1] iterate grnd for each seed                 --- !
+    ! --- [2] iterate grnd for each seed                 --- !
     ! ------------------------------------------------------ !
-    do k=1, OMPNumThreads
+    do k=1, nOMP
        multi_mt(0,k) = iand( seeds(k), maskF )
        do kmti=1, N-1
-          multi_mt(kmti,k) = int(maskI) * ( ieor( multi_mt(kmti-1,k), ( ishft( multi_mt(kmti-1,k), -30) ) ) ) + kmti
+          multi_mt(kmti,k) =  int(maskI) * ( ieor( multi_mt(kmti-1,k), ( ishft( multi_mt(kmti-1,k), -30) ) ) ) + kmti
           multi_mt(kmti,k) = iand( multi_mt(kmti,k), maskF )
        enddo
        multi_mti(k) = kmti
@@ -129,25 +149,31 @@ contains
   ! === random number generation                       === !
   ! ====================================================== !
   function multi_grnd( myth )
+    !$ use omp_lib
     implicit none
     integer                :: kk
-    integer, parameter     :: seed_default = 4357
-    integer                :: seeds(OMPNumThreads)
+    integer, allocatable   :: seeds(:)
     double precision       :: multi_grnd
     integer                :: y
+    integer                :: numthreads = 1
     integer, intent(in)    :: myth
 
     ! ------------------------------------------------------ !
     ! --- [1] iterate grnd for every thread              --- !
     ! ------------------------------------------------------ !
-    
     if ( multi_mti(myth) >= N ) then
        
        if ( multi_mti(myth) == N+1 ) then
-          do kk=1, OMPNumThreads
-             seeds(kk) = seed_default + kk
+          if ( nOMP == 0 ) then
+             !$omp parallel shared(numthreads)
+             !$ numthreads = omp_get_num_threads()
+             !$omp end parallel
+             nOMP = numthreads
+          endif
+          do kk=1, nOMP
+             seeds(kk) = default_seed + kk
           enddo
-          call multi_sgrnd( seeds )
+          call multi_sgrnd( seeds, nOMP )
        endif
        
        do kk=0, N-M-1
